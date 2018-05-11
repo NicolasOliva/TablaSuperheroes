94b5d4ab90ba952d04a0b4a472952a7488e20838
Nico ~ <nicko.oliva@gmail.com>
2018-05-11 19:29:57 -0300

    creacion de funciones nuevas en master.js y pagina update

--- master.js
+++ master.js
@@ -19,7 +19,7 @@ const Data = (method,url) => {
   Request(method,url)
     .then((res) => {
       if(res){
-        console.log(res);
+        return res;
       }
     })
     .catch((error) =>{
@@ -27,4 +27,31 @@ const Data = (method,url) => {
     })
 }
 
-Data("GET","data.json");
+var heroes = Data("GET","data.json");
+console.log(heroes);
+
+function BuscarID(heroes, id){   //se le pasa el json(heroes) y el id que se desea buscar
+    let flag = 0;
+      for(var i = 0; i < heroes.length && flag == 0; i++){
+        if(heroes[i].id == id){
+          flag = 1;
+          return heroes[i];
+        }
+      }
+      if (flag == 0){
+        alert("Heroe no encontrado");
+      }
+}
+
+function MostrarHeroe(heroe,elemento){  //se le pasa el heroe que se desea mostrar y el elemento html donde va a estar
+
+  //Esta funcion modifica el form del update.html con la informacion del heroe seleccionado
+
+  document.getElementById('nombre').innerHTML = ${heroe.nombre};
+  document.getElementById('alias').innerHTML = ${heroe.alias};
+  document.getElementById('bio').innerHTML = ${heroe.bio};
+  document.getElementById('universo').innerHTML = ${heroe.universo};
+  document.getElementById('enemigos').innerHTML = ${heroe.enemigos};
+  document.getElementById('romance').innerHTML = ${heroe.romance};
+
+}
