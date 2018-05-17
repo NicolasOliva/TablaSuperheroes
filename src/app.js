bc7c311d491cc1c2e880af101df9748d9f520cdd
Leandro Bisceglie <leam.90@gmail.com>
2018-05-16 22:10:53 -0300

    merge

--- src/app.js
+++ src/app.js
@@ -1,65 +1,28 @@
 import Api from '../lib/api.js'
-import ShowModal from './modal' //este es el modal que viene de modal.js
-import creation from './creation' // este es el crear ale
-import editHeroe from './update' // este es el update de nico
-import Delete from './delete' // eliminar de ale
-
-window.ShowModal = ShowModal;
+import ShowModal from './modal'
+import creation from './creation' 
+import editDataHero from './update' 
+import Delete from './delete' 
+import htmlTemplate from '../views/createModal.js'
+import editForm from '../views/editModal.js'
 
 window.Delete = Delete;
-
+window.editDataHero = editDataHero.EditarHeroe 
 window.creation = creation;
 
-window.ShowModal = () =>{
+window.createHero = () =>{
     let modal = document.getElementById('Cambiaso');
-    let htmlTemplate = `
-    <form id="formulario" class="form" action="" name="formulario" method="post">
-    <h2 class="form-titulo form-titulo1">Crea tu propio Heroe</h2>
-    <div class="form-container">
-            <div class="field">
-                <input type="text" class="form-text"  name="nombre" placeholder="Nombre" id="nombre">
-                <label for="nombre">Nombre</label>
-            </div>
-
-            <div class="field">
-                <input type="text" class="form-text"  name="alias" placeholder="Alias" id="alias" >
-                <label for="alias">Alias</label>
-            </div>
-
-            <div class="field">
-                <input type="text" class="form-text"  name="universe" placeholder="Universo" id="universe">
-                <label for="universe">Universo</label>
-            </div>
-
-            <div class="field">
-                <input type="text" class="form-text"  name="nemesis" placeholder="Nemesis" id="enemigos">
-                <label for="enemigos">Nemesis</label>
-            </div>
-
-            <div class="field">
-                <input type="text" class="form-text"  name="foto" placeholder="Foto" id="foto">
-                <label for="foto">Foto</label>
-            </div>
-
-            <div class="field">
-                <input type="text" class="form-text"  name="romance" placeholder="Romance" id="romance">
-                <label for="romance">Romance</label>
-            </div>
-
-            <div class="field bio-field">
-                <input type="text" class="form-text" name="bio" placeholder="Biografia" id="bio" >
-                <label for="bio">Biografia</label>
-            </div>
-
-            <input type="button" class="form-button" value="CREAR" id="btn" onclick="creation();">
-            
-        </div>
-    </form>                        
-    `
-
     modal.innerHTML = htmlTemplate;
     ShowModal();
 }
+
+window.editHero = (id) =>{
+    let modal = document.getElementById('Cambiaso');
+    modal.innerHTML = editForm;
+    ShowModal();
+    editDataHero.BuscarID(id);
+}
+
 
 const view = (viewHeroes) =>{
     let htmlTemplate = `                        
@@ -82,7 +45,7 @@ const view = (viewHeroes) =>{
                     <td>${viewHeroes.nemesis}</td>
                     <td><img src="${viewHeroes.foto}" alt="Smiley face" height="42" width="42"></td>
                     <td>${viewHeroes.romance}</td>
-                    <td><button onclick=editHeroe(${viewHeroes.id})>Editar</button><button onclick=Delete(${viewHeroes.id})>Borrar</button></td>
+                    <td><button onclick=editHero(${viewHeroes.id})>Editar</button><button onclick=Delete(${viewHeroes.id})>Borrar</button></td>
                 </tr> 
             </tbody>
             `;
---
+++ views/editModal.js
@@ -0,0 +1,44 @@
+
+const editForm = `
+    <form id="formulario" class="form" action="" name="formulario" method="post">
+        <h2 class="form-titulo form-titulo1">Crea tu propio Heroe</h2>
+        <div class="form-container">
+            <input type="text" class="form-text"  name="id" id="id" style="display:none;">
+            <div class="field">
+                <input type="text" class="form-text"  name="nombre" placeholder="Nombre" id="nombre">
+                <label for="nombre">Nombre</label>
+            </div>
+
+            <div class="field">
+                <input type="text" class="form-text"  name="alias" placeholder="Alias" id="alias" >
+                <label for="alias">Alias</label>
+            </div>
+
+            <div class="field">
+                <input type="text" class="form-text"  name="universe" placeholder="Universo" id="universe">
+                <label for="universe">Universo</label>
+            </div>
+
+            <div class="field">
+                <input type="text" class="form-text"  name="nemesis" placeholder="Nemesis" id="enemigos">
+                <label for="enemigos">Nemesis</label>
+            </div>
+
+            <div class="field">
+                <input type="text" class="form-text"  name="foto" placeholder="Foto" id="foto">
+                <label for="foto">Foto</label>
+            </div>
+
+            <div class="field">
+                <input type="text" class="form-text"  name="romance" placeholder="Romance" id="romance">
+                <label for="romance">Romance</label>
+            </div>
+
+            <div class="field bio-field">
+                <input type="text" class="form-text" name="bio" placeholder="Biografia" id="bio" >
+                <label for="bio">Biografia</label>
+            </div>
+            <input type="button" class="form-button" value="EDITAR" id="btn" onclick="editDataHero()">  
+        </div>
+    </form> `;
+export default editForm;
