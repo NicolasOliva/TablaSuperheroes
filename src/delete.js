e1329735fcff1706d28e8618a85d2c4de3956f92
lagarkill <apachai.ho@hotmail.com>
2018-05-22 16:37:31 -0300

    provando cosas

--- src/delete.js
+++ src/delete.js
@@ -1,22 +1,8 @@
 import Api from '../lib/api.js'
 import App from './app.js'
+
 const Delete = (id)=>{
     console.log('el id es',id)
-    window.DeleteHero = () => {
-        let modal = document.getElementById('Cambiaso');
-        let htmlTemplate2 = `
-        <div>
-            <h3>Estas seguro?</h3>
-            <div>
-                <button onclick=Delete(id)>Si</button>
-                <button class="close">No</button>
-            </div>
-        </div>
-        `;
-        modal.innerHTML = htmlTemplate2;
-        ShowModal();
-    }
-    
     Api("GET","data.json")//a futuro cambiar a DELETE
         .then((hero) => {
         if(hero){
@@ -34,7 +20,7 @@ const Delete = (id)=>{
             alert("Error al buscar la informacion");
         })
 
-
+    return confirm('')
 
 };
 
